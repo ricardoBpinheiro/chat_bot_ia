daf07be0cec796293b861469b13731db34a87bf8
ricardoBpinheiro <ricardo.pinheiro@catolicasc.org.br>
2021-04-18 15:44:52 -0300

    Chat Bot - Machine learning

---
+++ main.py
@@ -0,0 +1,131 @@
+# pip install nltk
+# pip install tensorflow
+# pip install numpy
+# pip install tflearn
+
+# conda activate chatbot_ia
+
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy
+import tflearn
+import tensorflow
+import random
+import json
+import pickle
+
+with open("intents.json") as file:
+	data = json.load(file)
+
+try:
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+        	# wrds = nltk.word_tokenize(pattern, language='portuguese')
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+
+    labels = sorted(labels)
+
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+        
+        wrds = [stemmer.stem(w.lower()) for w in doc]
+        
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+
+        training.append(bag)
+        output.append(output_row)
+
+
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
+
+tensorflow.compat.v1.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+# Verifica se tem um modelo existente para nao repetir o treinamento
+try:
+   model.load("model.tflearn")
+except:
+	model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+	model.save("model.tflearn")
+
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+    
+    # s_words = nltk.word_tokenize(s, language='portuguese')
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+    
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+            
+    return numpy.array(bag)
+    
+    
+def chat():
+    print("Comece a falar! (Para sair digite sair)")
+    while True:
+        inp = input("Você: ")
+        if inp.lower() == "quit":
+            break
+        
+        # Faz a comparação de probabilidade de todos os valores na lista (Cada neuronio do modelo)
+        results = model.predict([bag_of_words(inp, words)])
+        # print(results)
+
+        results_index = numpy.argmax(results)
+        tag = labels[results_index]
+        # print(tag)
+      
+        for tg in data["intents"]:
+            if tg["tag"] == tag:
+                responses = tg["responses"]
+                
+        print(random.choice(responses))
+        
+
+chat()
